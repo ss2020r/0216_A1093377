22f12e95dc6ade1c318d464747d1ae4bae1e1f17
ss2020r <a1093377@mail.nuk.edu.tw>
2023-02-16 21:36:42 +0800

    基礎架構

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<html>
+    <head>
+       <meta charset="UTF-8">
+               <title>高大資管</title>
+    </head>
+    <body>
+        hello html 
+       <br/>
+        <?php echo "Hello, World\n";?>
+       
+    </body>
+
+</html>
+
+
